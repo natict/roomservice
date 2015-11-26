1ec2c302cfea8f78241bdb4080c4d03b8d0370bf
nir0s <nir36g@gmail.com>
2015-11-26 18:34:11 +0200

    cleanup

--- roomservice/rms.py
+++ roomservice/rms.py
@@ -14,22 +14,11 @@ def _get_os_props():
     return distro.lower(), release.lower()
 
 
-def get_meminfo():
-    with open('/proc/meminfo', 'r') as f:
-        for l in f.read():
-            data = l.split(' ')
-            print data
-
-
 @click.command()
 @click.option('-p', '--port', default=31337)
 @click.option('--debug', default=False, is_flag=True)
 @click.option('-r', '--run-command', type=str, required=False)
 def main(port, debug, run_command):
-    if run_command:
-        if hasattr(globals(), run_command):
-            getattr(globals(), run_command)
-    else:
         app = Flask('roomservice')
         api = Api(app)
 

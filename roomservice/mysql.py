345a64c03effd2f349e6304b5aebe0a495b0c903
Michael Sverdlik <mhsver@gmail.com>
2015-11-26 20:15:26 +0200

    yay

--- roomservice/mysql.py
+++ roomservice/mysql.py
@@ -2,22 +2,23 @@ import pymysql
 from flask_restful import Resource
 from flask import abort
 
+ALLOWED_SHOW = ('processlist', 'databases', 'plugins', 'privileges')
+
+
 class Mysql(Resource):
     def __init__(self):
         self.connection = pymysql.connect(user='root')
         self.cursor = self.connection.cursor()
 
-    def show_processlist(self):
-        self.cursor.execute('SHOW PROCESSLIST')
-
+    def _execute(self, sql):
+        self.cursor.execute(sql)
         desc_id = tuple(x[0] for x in self.cursor.description)
         query_result = self.cursor.fetchall()
         results = [dict(zip(desc_id, item)) for item in query_result]
         return results
 
-
     def get(self, cmd):
-        if cmd == 'processlist':
-            return self.show_processlist()
+        if cmd in ALLOWED_SHOW:
+            return self._execute('show ' + cmd)
         else:
             abort(400)

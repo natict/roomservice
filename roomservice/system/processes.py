cf29e0288fad4cac95b48b824d5d83b7abdd65d3
Nati CT <nocoot@gmail.com>
2015-11-26 19:57:10 +0200

    add environ to process information if available

--- roomservice/system/processes.py
+++ roomservice/system/processes.py
@@ -5,15 +5,25 @@ from flask_restful import Resource
 from psutil import NoSuchProcess
 
 
+def _process_enrich(p):
+    process_dict = p.as_dict()
+    try:
+        environ = open('/proc/{pid}/environ'.format(pid=process_dict.get('pid'))).read()
+        process_dict['environ'] = {var.split('=', 1)[0]: var.split('=', 1)[1] for var in environ.split('\0') if var}
+    except Exception:
+        pass  # no environ for you!
+    return process_dict
+
+
 class Processes(Resource):
     def get(self):
-        return [p.as_dict() for p in psutil.process_iter()]
+        return [_process_enrich(p) for p in psutil.process_iter()]
 
 
 class Process(Resource):
     def get(self, pid):
         try:
-            return psutil.Process(pid=pid).as_dict()
+            return _process_enrich(psutil.Process(pid=pid))
         except NoSuchProcess:
             return '', 404
 

c884453c0a10cbdc6e64d3f4ea27063ab994fbfa
nir0s <nir36g@gmail.com>
2015-11-26 17:51:08 +0200

    added os properties identifier

--- main.py
+++ main.py
@@ -3,10 +3,17 @@
 from flask import Flask
 from flask_restful import Api, Resource
 import click
+import platform
 
 
 app = Flask('roomservice')
 api = Api(app)
+
+
+def _get_os_props():
+    distro, _, release = platform.linux_distribution(
+        full_distribution_name=False)
+    return distro.lower(), release.lower()
 
 
 @click.command()

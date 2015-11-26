36eda1a7e99369ecde6b4c89f63c1ac19d2dc4a8
nir0s <nir36g@gmail.com>
2015-11-26 18:20:41 +0200

    fixes

---
+++ roomservice/rms.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import click
+import platform
+
+from flask import Flask
+from flask_restful import Api
+
+
+def _get_os_props():
+    distro, _, release = platform.linux_distribution(
+        full_distribution_name=False)
+    return distro.lower(), release.lower()
+
+
+def get_meminfo():
+    with open('/proc/meminfo', 'r') as f:
+        for l in f.read():
+            data = l.split(' ')
+            print data
+
+
+@click.command()
+@click.option('-p', '--port', default=31337)
+@click.option('--debug', default=False, is_flag=True)
+@click.option('-r', '--run-command', type=str, required=False)
+def main(port, debug, run_command):
+    if run_command:
+        if hasattr(globals(), run_command):
+            getattr(globals(), run_command)
+    else:
+        app = Flask('roomservice')
+        api = Api(app)
+
+        app.run(port=port, debug=debug)
+
+
+if __name__ == '__main__':
+    main()
